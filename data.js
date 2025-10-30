201d7e91944eea6967719da4e2cc4c974189e0df
Danieldavidoh <dh501502@gmail.com>
2025-10-30 06:32:16 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+const cities = [
+  'Mumbai', 'Pune', 'Nagpur', 'Nashik', 'Thane', 'Aurangabad', 'Solapur', 'Amravati', 'Nanded', 'Kolhapur',
+  'Akola', 'Latur', 'Ahmadnagar', 'Jalgaon', 'Dhule', 'Ichalkaranji', 'Malegaon', 'Bhusawal', 'Bhiwandi', 'Bhandara'
+  // ... (전체 90개 생략 – 아래 전체 붙여!)
+];
+const coords = {
+  'Mumbai': [19.07, 72.88], 'Pune': [18.52, 73.86], 'Nagpur': [21.15, 79.08], 'Nashik': [20.00, 73.79],
+  'Thane': [19.22, 72.98], 'Aurangabad': [19.88, 75.34], 'Solapur': [17.67, 75.91], 'Amravati': [20.93, 77.75],
+  'Nanded': [19.16, 77.31], 'Kolhapur': [16.70, 74.24], 'Akola': [20.70, 77.00], 'Latur': [18.40, 76.57],
+  'Ahmadnagar': [19.10, 74.75], 'Jalgaon': [21.00, 75.57], 'Dhule': [20.90, 74.77], 'Ichalkaranji': [16.69, 74.47],
+  'Malegaon': [20.55, 74.53], 'Bhusawal': [21.05, 76.00], 'Bhiwandi': [19.30, 73.06], 'Bhandara': [21.17, 79.65]
+  // ... (나머지 70개도 동일하게)
+};
